4217c6d77c22bb42dfd0bca33a811772c08b72e7
Olivia Park <olivia.park1346@gmail.com>
2022-07-30 17:54:23 -0500

    Add mui

---
+++ pages/about.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next';
+import styles from '../styles/Home.module.css'
+
+const About: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        About Us
+      </h1>
+    </div>
+  )
+}
+
+export default About
